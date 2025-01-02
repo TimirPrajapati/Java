06a16531ce31b587aac8d2981555fa7aa78adf30
Timir Prajapati <30820183+TimirPrajapati@users.noreply.github.com>
2025-01-02 18:55:46 -0500

    Added some demo projects

---
+++ KeywordDemo/src/TestJavaKeyword.java
@@ -0,0 +1,12 @@
+
+public class TestJavaKeyword {
+
+	public static void main(String[] args) {
+		// Note "this" is a keyword in java
+		String this = "Hello World!";
+		System.out.println(this);
+		
+		// this program gives an error because this is keyword in java so we can not use it in variable.
+	}
+
+}

94f5db68a10b88d3e4431dc8af388cd22edc6771
Timir Prajapati <30820183+TimirPrajapati@users.noreply.github.com>
2025-01-02 19:30:04 -0500

    Java Data Types Demo

---
+++ DataTypesDemo/src/TestJavaDataTypes.java
@@ -0,0 +1,29 @@
+
+public class TestJavaDataTypes {
+
+	public static void main(String[] args) {
+		// There are two types of Datatypes in Java.
+		// Primitive and Non-Primitive
+		
+		byte myByte = 100;			 // Byte
+		int myNum = 5;               // Integer (whole number)
+		long myLong = 12121;         // Long
+		short myS = 56;              // Short 
+		float myFloatNum = 5.99f;    // Floating point number
+		double myD = 4.355453532;    // Double 
+		char myLetter = 'D';         // Character
+		boolean myBool = true;       // Boolean
+		String myText = "Hello";     // String
+		
+		System.out.println(myByte);
+		System.out.println(myNum);
+		System.out.println(myLong);
+		System.out.println(myS);
+		System.out.println(myFloatNum);
+		System.out.println(myD);
+		System.out.println(myLetter);
+		System.out.println(myBool);
+		System.out.println(myText);
+	}
+
+}

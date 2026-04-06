a4dc9a50b82dc013e5f159c2fdb576ab599d2b5a
Timir Prajapati <30820183+TimirPrajapati@users.noreply.github.com>
2026-04-05 22:38:40 -0400

    Learning session 33 part-1

---
+++ Introduction/src/Locators.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.id("inputUsername")).sendKeys("Jack");
+		driver.findElement(By.name("inputPassword")).sendKeys("Jack@123");
+		driver.findElement(By.className("signInBtn")).click();
+		
+	}
+
+}

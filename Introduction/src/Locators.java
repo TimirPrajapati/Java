a3f5bb9a71ae5c7550ef0d21bcf89338f29af6c1
Timir Prajapati <30820183+TimirPrajapati@users.noreply.github.com>
2026-04-07 23:54:36 -0400

    Learning sessions 34, 35, 36 covered with example

--- Introduction/src/Locators.java
+++ Introduction/src/Locators.java
@@ -1,3 +1,5 @@
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,11 +9,14 @@ public class Locators {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
+		// implicit wait -- 5 seconds time out.
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys("Jack");
 		driver.findElement(By.name("inputPassword")).sendKeys("Jack@123");
 		driver.findElement(By.className("signInBtn")).click();
-		
+		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+
 	}
 
 }

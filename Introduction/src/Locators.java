d61950282630cf65edd801d4e92eade2a885137f
Timir Prajapati <30820183+TimirPrajapati@users.noreply.github.com>
2026-04-09 23:52:44 -0400

    lesson 37 finished

--- Introduction/src/Locators.java
+++ Introduction/src/Locators.java
@@ -17,6 +17,7 @@ public class Locators {
 		driver.findElement(By.className("signInBtn")).click();
 		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 		driver.findElement(By.linkText("Forgot your password?")).click();
+		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Jack");
 	}
 
 }
